8ce8d4d1226f1a03d414bc4511d35692447e4515
Stanley Huang <huang.stanley050@gmail.com>
2020-04-19 11:02:47 +1000

    start settting email up

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 import serverless from "serverless-http";
 import express from "express";
+import cron from "node-cron";
+import nodeMailer from "nodemailer";
 
 const app = express();
+
+const transporter = nodeMailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "ewgodhand@gmail.com",
+    pass: process.env.EMAIL_PASSWD,
+  },
+});
+const mailOptions = {
+  from: "ewgodhand@gmail.com",
+  to: "infamous_godhand@yahoo.com",
+  subject: `Daily Email reminder`,
+  text: `Hi there, this email was automatically sent by us`,
+};
 
 app.get("/", (req, res) => {
   return res.send("Hello World!");
